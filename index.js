bbffed4e24523c88c72c22c76281adc5f124d877
zhalok-avenirip <zhalok@aveniripltd.com>
2023-11-20 19:18:34 +0600

    updated with email interface

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-import { MarkerArea } from "markerjs2";
+import zIndex from "@mui/material/styles/zIndex";
+import html2canvas from "html2canvas";
 
+import { MarkerArea } from "markerjs2";
 
 const takeScreenShot = async (img) => {
   const stream = await navigator.mediaDevices.getDisplayMedia({
@@ -9,9 +11,8 @@ const takeScreenShot = async (img) => {
   const track = stream.getVideoTracks()[0];
 
   const imageCapture = new ImageCapture(track);
-  // take first frame only
+
   const bitmap = await imageCapture.grabFrame();
-  console.log(bitmap);
 
   track.stop();
 
@@ -33,9 +34,9 @@ function showMarkerArea(target) {
   console.log(markerArea);
   markerArea.settings.displayMode = "popup";
   markerArea.uiStyleSettings.zIndex = "1000";
-  // markerArea.pos;
+  // markerArea.pos;a
 
-  markerArea.uiStyleSettings.canvasBackgroundColor = "rgba(0,0,0,0.5)";
+  markerArea.uiStyleSettings.backgroundColor = "rgba(0,0,0,0.5)";
   // markerArea.uiStyleSettings
   markerArea.addEventListener("render", (event) => {
     // console.log(event);
@@ -54,8 +55,9 @@ function downloadImage(byteString, fileName) {
   URL.revokeObjectURL(byteString);
 }
 
-function annotate() {
-  
+export default function annotate() {
+  //@ts-ignore
+  // const dom = document.body;
   const root = document.getElementById("root");
 
   const preMain = document.getElementById("main");
@@ -63,6 +65,23 @@ function annotate() {
 
   const main = document.createElement("div");
   main.id = "main";
+
+  var tool = document.createElement("div");
+
+  // Set its inner HTML
+  // tool.innerHTML = "I'm a sticky block!";
+
+  // Set the CSS properties
+  tool.style.position = "fixed";
+  tool.style.top = "0px";
+  tool.style.left = "0px";
+  tool.style.width = "100%";
+  tool.style.zIndex = "1000";
+  tool.style.backgroundColor = "#f9f9f9";
+  tool.style.padding = "10px";
+  tool.style.zIndex = "1000";
+
+  // document.body.appendChild(tool);
 
   const annotatorModal = document.createElement("div");
 
@@ -95,6 +114,80 @@ function annotate() {
   annotator.style.overflowY = "scroll";
   annotator.style.zIndex = "1000";
 
+  const emailDiv = document.createElement("div");
+  emailDiv.id = "email";
+  emailDiv.style.marginTop = "20px";
+  emailDiv.style.display = "none";
+  emailDiv.style.flexDirection = "column";
+  emailDiv.style.justifyContent = "center";
+
+  const recipient = document.createElement("input");
+  recipient.type = "email";
+  recipient.placeholder = "Recipient";
+  recipient.style.marginBottom = "10px";
+  recipient.style.padding = "10px";
+  recipient.style.borderRadius = "5px";
+  recipient.style.border = "1px solid #ccc";
+
+  // recipient.onchange = (e) => {
+  //   const val = e.target.value;
+  //   const emails = val.split(" ");
+  //   emails.forEach((email) => {
+  //     // Assuming recipient is an input element for the email
+  //     const badge = document.createElement("span");
+  //     badge.textContent = email; // Set the text for the badge
+  //     badge.style.backgroundColor = "green"; // Set the background color for the badge
+  //     badge.style.color = "white"; // Set the text color for the badge
+  //     badge.style.borderRadius = "5px"; // Make the badge round
+  //     badge.style.padding = "2px 5px"; // Add some padding to the badge
+  //     badge.style.marginLeft = "10px"; // Add some space between the email and the badge
+
+  //     recipient.parentNode.insertBefore(badge, recipient.nextSibling);
+  //   });
+  // };
+
+  const subject = document.createElement("input");
+  subject.type = "text";
+  subject.placeholder = "Subject";
+  subject.style.marginBottom = "10px";
+  subject.style.padding = "10px";
+  subject.style.borderRadius = "5px";
+  subject.style.border = "1px solid #ccc";
+
+  const message = document.createElement("textarea");
+  message.placeholder = "Message";
+  message.style.marginBottom = "10px";
+  message.style.padding = "10px";
+  message.style.borderRadius = "5px";
+  message.style.border = "1px solid #ccc";
+  message.style.height = "200px";
+
+  const sendEmailButton = document.createElement("button");
+  sendEmailButton.innerHTML = "Send Email";
+  sendEmailButton.style.marginRight = "auto";
+  sendEmailButton.style.width = "30%";
+  sendEmailButton.style.marginTop = "10px";
+  sendEmailButton.onclick = () => {
+    const email = recipient.value;
+    const sub = subject.value;
+    const msg = message.value;
+    const img = document.getElementById("annotate_preview");
+
+    const data = {
+      email,
+      sub,
+      msg,
+      img,
+    };
+
+    console.log(data);
+  };
+
+  emailDiv.appendChild(recipient);
+  emailDiv.appendChild(subject);
+  emailDiv.appendChild(message);
+  emailDiv.appendChild(sendEmailButton);
+
   const buttonsDiv = document.createElement("div");
   buttonsDiv.style.display = "flex";
   buttonsDiv.style.flexDirection = "row";
@@ -107,18 +200,8 @@ function annotate() {
 
   root?.appendChild(main);
 
-  // const img = document.createElement("img");
   const img = document.createElement("img");
   img.id = "annotate_preview";
-
-  // img.style.border = "1px solid #ccc";
-  // img.width = "100%";
-  // // img.height = "fit-content";
-  // img.height = "100%";
-  // img.style.overflowY = "scroll";
-  //   img.style.marginBottom = "10px";
-  // img.style.overflowY = "auto";
-  //   img.style.borderRadius = "10px";
 
   img.onclick = (e) => {
     showMarkerArea(e.target);
@@ -127,6 +210,9 @@ function annotate() {
   const openButton = document.createElement("button");
   openButton.id = "open";
   openButton.innerHTML = "Take Screenshot";
+
+  tool.appendChild(openButton);
+  document.body.appendChild(tool);
 
   const closeButton = document.createElement("button");
   closeButton.id = "close";
@@ -143,10 +229,16 @@ function annotate() {
     downloadImage(img.src, "screenshot.png");
   };
 
+  const emailButton = document.createElement("button");
+  emailButton.id = "emailButton";
+  emailButton.innerHTML = "Email";
+  emailButton.style.marginRight = "left";
+  emailButton.style.width = "50%";
+  emailButton.onclick = () => {
+    emailDiv.style.display = "flex";
+  };
+
   const openImage = () => {
-    // takeScreenshot(dom, img).then(() => {
-    //   annotatorModal.style.display = "flex";
-    // });
     takeScreenShot(img).then(() => {
       annotatorModal.style.display = "flex";
     });
@@ -160,10 +252,12 @@ function annotate() {
     annotatorModal.style.display = "none";
   };
 
+  buttonsDiv.appendChild(emailButton);
   buttonsDiv.appendChild(downloadButton);
   buttonsDiv.appendChild(closeButton);
 
   annotator.appendChild(img);
+  annotator.appendChild(emailDiv);
   annotator.appendChild(buttonsDiv);
 
   annotatorModal.appendChild(annotator);
@@ -199,5 +293,3 @@ function annotate() {
 
   return "";
 }
-
-export default annotate;
